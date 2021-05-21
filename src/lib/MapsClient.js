b916351a322aecf1e9a87874115c598961dc6af0
louis-sanchez <l.sanchez@outlook.com>
2021-05-21 03:11:01 +0200

    Check weather forecast for address
    This only takes care of the happy path scenario

--- src/lib/MapsClient.js
+++ src/lib/MapsClient.js
@@ -1,9 +1,6 @@
 const fetch = require('node-fetch')
 const MAPS_API = process.env.npm_package_config_maps_api
 
-const HTTP_OK = 200
-const HTTP_NOT_FOUND = 404
-
 class MapClient {
 
     async search(address) {
@@ -24,22 +21,9 @@ class MapClient {
         const urlParams = Object.entries(config).map(e => e.join('=')).join('&')
         const request = `${MAPS_API}/search?${addressParams}&${urlParams}`
 
-
-        const options = {
-            method: 'get',
-            body: JSON.stringify(address),
-            headers: { 'Content-Type': 'application/json' },
-        }
-
         return fetch(request)
             .then(checkStatus)
             .then(res => res.json())
-            .then(json => {
-                if (json.length)
-                    return HTTP_OK
-                else
-                    return HTTP_NOT_FOUND
-            })    
     }
 }
 
--- src/lib/app.js
+++ src/lib/app.js
@@ -1,14 +1,53 @@
 const { search } = require('./MapsClient')
+const { forecast } = require('./WeatherClient')
+
 const express = require('express')
+
+const HTTP_OK = 200
+const HTTP_NOT_FOUND = 404
 
 const app = express()
     .use(express.json())
     .use(express.urlencoded({ extended: true }))
 
 app.post('/address', async ({ body }, res) => {
+
     const address = { ...body }
-    const status = await search(address)
-    res.sendStatus(status)
+    console.log(address)
+    const response = await search(address)
+
+    if (response.length) {
+        console.log(response)
+        res.sendStatus(HTTP_OK)
+    }
+    else
+        res.sendStatus(HTTP_NOT_FOUND)
+    
 })
+
+app.post('/weather', async ({ body }, res) => {
+    const address = { ...body }
+     const response = await search(address)
+
+    if (response.length) {
+        console.log(response)
+        const lat  = response[0].lat
+        const lon  = response[0].lon
+
+        console.log("lat", lat)
+        console.log("lon", lon)
+
+        const weather = await forecast(lat, lon)
+        console.log(weather)
+
+        res.sendStatus(HTTP_OK)
+    }
+    else
+        res.sendStatus(HTTP_NOT_FOUND)
+})
+
+const isEmpty = (obj) => {
+    return (Object.entries(obj).length === 0 && obj.constructor === Object)
+}
 
 module.exports = app
